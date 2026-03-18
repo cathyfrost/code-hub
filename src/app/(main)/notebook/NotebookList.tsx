72c60ccb91e3547be6d19bc81772c71dd968b1c8
zgyutong <zgyutong@gmail.com>
2026-03-19 07:57:43 +0800

    online chatting

--- src/app/(main)/notebook/NotebookList.tsx
+++ src/app/(main)/notebook/NotebookList.tsx
@@ -26,7 +26,7 @@ import {
 } from "lucide-react";
 import { formatDistanceToNow } from "date-fns";
 import { zhCN } from "date-fns/locale";
-import { useToast } from "@/hooks/use-toast";
+import { useToast } from "@/components/ui/use-toast";
 import {
   Dialog,
   DialogContent,
